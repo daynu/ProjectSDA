ae17320cf1d5c5515f802b940e5ddedfde0ec2c0
Dan <danu111gribincea@gmail.com>
2023-11-20 12:09:37 +0200

    created user options menu

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,12 +3,15 @@ import { useAuth } from "../utils/AuthContext"
 import { useEffect } from "react"
 import SearchEventsBar from "./SearchEventsBar"
 import { useEvents } from "../utils/EventsProvider"
+import { useState } from "react"
 
 
 export default function Navbar()
 {
 
     const {user, logout} = useAuth()
+
+    const [showUserOptions, setShowUserOptions] = useState(false)
 
     const events = useEvents()
 
@@ -18,16 +21,28 @@ export default function Navbar()
         console.log("pressed")
    }
 
+   function handleUserNameClick()
+   {
+    setShowUserOptions(!showUserOptions)
+   }
 
     return(
         <div id="navbar">
             <Link id = "logo" to="/">EventBV</Link>
             <SearchEventsBar events={events} />
             {user.name ? (
-                <div id = "loginSignup">
-                <p id="userName">{user.name}</p>
-                <button id = "logout" onClick = {LogOut}>Log out</button>
-                </div>
+                <>
+                    <div id = "userName">
+                        <p onClick={handleUserNameClick} id="userName">{user.name}</p>
+                        
+                            {showUserOptions && 
+                            <div id="userOptions">
+                                <button id = "logout" onClick = {LogOut}>Log out</button>
+                            </div>}
+                        
+                    </div>
+                    
+                </>
                 )
             :
             (   <div id = "loginSignup">
