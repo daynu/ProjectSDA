c3e4276aa46224f3aa04dcf7c7be8c969288ef55
Dan <danu111gribincea@gmail.com>
2023-11-27 21:05:51 +0200

    minor event page improvements

--- client/src/components/EventPage.js
+++ client/src/components/EventPage.js
@@ -6,6 +6,7 @@ import LoadingScreen from '../img/BrasovStema.png'
 import dateToString from "../utils/dateFormatter";
 import Calendar from '../img/date.png'
 import Location from '../img/pin.png'
+import Clock from '../img/clock.png'
 
 function EventPage()
 {
@@ -32,22 +33,24 @@ function EventPage()
         </div>
         <div className="eventPage">
             <div className="eventView">
-            <div className="titleAndDescription">
-                <h2>{eventData.title}</h2>
-                <p>{eventData.organizer}</p>
+            <div className="eventDescription">
+                <h2>Despre</h2>
+                <p>Organizator: {eventData.organizer}</p>
                 <p>{eventData.description}</p>
-                <a href={eventData.link ? eventData.link : "https://www.youtube.com/watch?v=NpkPy3y6ors"} target="_blank"><button>Cumpără bilet</button></a>
             </div>
             
-            <div className="eventPageContainer">
+            <div className="ticket">
                 <div className="eventDetails">
-                    <h2>{eventData.title}</h2>
+                    <h2 className="eventCellTitle">{eventData.title}</h2>
                     <div className="eventCellDate"><img className="eventIcon" alt="calendar" src={Calendar}/><p>{dateToString(eventData.date)}</p></div>
-                    <div className="eventCellLocation"><img src={Location} className="eventIcon"/><p>{eventData.location}</p></div>
+                    <div className="eventCellLocation"><img src={Location} className="eventIcon" alt="pin"/><p>{eventData.location}</p></div>
+                    <div className="eventCellHour"><img src={Clock} className="eventIcon" alt="clock"/><p>{eventData.hour}</p></div>
                 </div>
                 
-                <div className="buyTicketContainer"><a href={eventData.link ? eventData.link : "https://www.youtube.com/watch?v=NpkPy3y6ors"} target="_blank"><button>Cumpără bilet</button></a></div>
-                
+            <div className="buyTicketContainer">
+                <a id="buyTicketLink" href={eventData.link ? eventData.link : "https://www.youtube.com/watch?v=NpkPy3y6ors"} target="_blank"><button id="buyTicket">Cumpără bilet</button></a>
+                <button id="interestedButton">Interesat</button>
+                </div>
             </div>
         </div>
         
