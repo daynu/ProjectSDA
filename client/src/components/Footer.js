a51bc12f817df13d9106affc1a44d537b95090f9
Dan <danu111gribincea@gmail.com>
2023-11-30 17:15:12 +0200

    added footer options

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,6 +4,12 @@ export default function Footer()
     return(
         <footer>
         <div class="footer-content">
+          <h3 id="footerLogo">Bvent</h3>
+          <div id="footerOptions">
+            <p>Devino Organizator</p>
+            <p>Despre</p>
+            <p>Contact</p>
+          </div>
           <p>&copy; 2023 Pixel. All rights reserved.</p>
         </div>
       </footer>
