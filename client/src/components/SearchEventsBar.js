209c8fd2d9f2f7885e1d5ad985fbaf2ae8f42e14
Dan <danu111gribincea@gmail.com>
2023-11-30 22:24:13 +0200

    added keyboard navigation for search bar

--- client/src/components/SearchEventsBar.js
+++ client/src/components/SearchEventsBar.js
@@ -1,10 +1,11 @@
-import { useState } from "react";
-
-import Magnifying from '../img/Magnifying.svg.png'
+import { useState, useRef } from "react";
+import Magnifying from '../img/Magnifying.svg.png';
 
 function SearchEventsBar({ events }) {
   const [search, setSearch] = useState('');
   const [searchEvents, setSearchEvents] = useState([]);
+  const [selectedEvent, setSelectedEvent] = useState(-1);
+
 
   function handleSearchChange(e) {
     const updatedSearch = e.target.value;
@@ -14,30 +15,49 @@ function SearchEventsBar({ events }) {
     setSearchEvents(results);
   }
 
+  function handleKeyDown(e) {
+
+    if(e.key === 'ArrowUp' && selectedEvent > 0)
+    {
+      setSelectedEvent(prev => prev - 1)
+    }
+    else if(e.key === 'ArrowDown' && selectedEvent < searchEvents.length - 1)
+    {
+      setSelectedEvent(prev => prev + 1)
+    }
+    else if(e.key === 'Enter' && selectedEvent >= 0)
+    {
+      window.open(`/event/${searchEvents[selectedEvent]._id}`, '_self')
+    }
+  }
+
   return (
     <div id="searchBar">
       <div id="searchMain">
-        <img src={Magnifying} alt="magnifying"/>
-        <input id="searchArea" onChange={handleSearchChange} type="text" placeholder="Caută evenimente" />
+        <img src={Magnifying} alt="magnifying" />
+        <input
+          id="searchArea"
+          onChange={handleSearchChange}
+          onKeyDown={handleKeyDown}
+          type="text"
+          placeholder="Caută evenimente"
+        />
       </div>
-      
+
       {searchEvents.length > 0 && search !== '' ? (
         <div id="searchedEvents">
-          {searchEvents.map((event) => (  
-            <a href={`/event/${event._id}`}>
-            <div className="searchEventDiv" key={event.id}>
+          {searchEvents.map((event, index) => (
+            <a href={`/event/${event._id}`} key={index}>
+              <div className= {`searchEventDiv ${selectedEvent === index ? 'focused': ''}`} id={event._id}>
                 <p className="foundEvent">{event.title}</p>
-            </div>
+              </div>
             </a>
           ))}
         </div>
-      ):
-      search !== '' &&
-      (
-        
-          <div id="searchedEvents">
-                <p id="noEventSearch">Niciun eveniment găsit</p>
-            </div>
+      ) : search !== '' && (
+        <div id="searchedEvents">
+          <p id="noEventSearch">Niciun eveniment găsit</p>
+        </div>
       )}
     </div>
   );
